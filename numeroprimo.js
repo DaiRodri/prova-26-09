27774b8ff44e56cae5c8a66d7b62f201ed449ec1
DaiRodri <daianarodri@gmail.com>
2024-09-26 21:31:51 -0300

    Add files via upload

---
+++ numeroprimo.js
@@ -0,0 +1,9 @@
+//Crie um programa que exiba em uma tabela e destaque todos os números primos que estão entre 0 e 200.
+
+let numero = 0
+
+for (i =o ; i <=200 ; i++){
+
+    if( numero [i] / numero [i] == 1)
+    console.log(numero[i])
+}
---
+++ questão8.js
@@ -0,0 +1,8 @@
+//Crie um programa em JavaScript que solicite ao usuário 10 títulos de notícias de sua cidade e armazene-os em um array. Após inserir todos os títulos, exiba-os na tela na ordem
+ //inversa (do mais recente para o mais antigo)
+
+ let noticias = ['festa','politica','comida','lazer','economia','tributos','saude','transito','transporte','segurança']
+ noticias.reverse ()
+ console.log(noticias)
+
+ 
